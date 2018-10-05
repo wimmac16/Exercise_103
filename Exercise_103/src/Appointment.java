ad1562045776faabdf3eca75bcbc0f0e5497ac6a
wimmac <wimmac16@new.edvhtl.at>
2018-10-05 18:51:10 +0200

    implemented class Appointment

--- Exercise_103/src/Appointment.java
+++ Exercise_103/src/Appointment.java
@@ -1,13 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author mwimm
- */
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Appointment {
+
+    private LocalDate date;
+    private LocalTime time;
+    private String text;
+
+    public Appointment(LocalDate date, LocalTime time, String text) {
+        this.date = date;
+        this.time = time;
+        this.text = text;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public String getText() {
+        return text;
+    }
     
+    @Override
+    public String toString(){
+        return String.format("%s - %s --> %s", date,time,text);
+    }
 }
