565062594224b637e2f822157009976b8b05d4b7
wimmac <wimmac16@new.edvhtl.at>
2018-10-05 18:57:13 +0200

    implemented methods add, remove, getSize and getElementAt

--- Exercise_103/src/AppointmentModell.java
+++ Exercise_103/src/AppointmentModell.java
@@ -1,13 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author mwimm
- */
-public class AppointmentModell {
+import java.util.ArrayList;
+import javax.swing.AbstractListModel;
+
+
+public class AppointmentModell extends AbstractListModel{
+    
+    private ArrayList<Appointment> appointments=new ArrayList<>();
+    @Override
+    public int getSize() {
+        return appointments.size();
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+       return appointments.get(index);
+    }
+    
+    public void add(Appointment a){
+        appointments.add(a);
+        fireIntervalAdded(appointments.size()-1, appointments.size()-1, 0);
+    }
+    
+    public void remove(int i){
+        appointments.remove(i);
+        fireContentsChanged(i, i, i);
+    }
     
 }
